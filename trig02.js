738aeb797e9f1d186f3913f27912b5938ecbbcc6
iseneld <84731866+iseneld@users.noreply.github.com>
2022-03-07 20:40:25 +0100

    Animating color, position and size of circle.

--- trig02.js
+++ trig02.js
@@ -6,16 +6,26 @@ window.onload = function () {
 
   let centerY = height * 0.5,
     centerX = width * 0.5,
-    offset = height * 0.4,
-    speed = 0.05,
+    offset = width / 4,
+    baseRadius = width / 3,
+    baseAlpha = 0.52,
+    offsetAlpha = 0.1,
+    speed = 0.01,
     angle = 0;
 
   function render() {
-    let y = centerY + Math.sin(angle) * offset;
+    let y = centerY * Math.sin(angle) + baseRadius;
+    radius = baseRadius + Math.sin(angle) * offset;
+    alpha = baseAlpha + Math.sin(angle) * offsetAlpha;
 
+    context.lineWidth = 40;
+    context.strokeStyle = "white";
+    context.fillStyle =
+      "rgba(" + y + ", " + radius / 2 + ", " + angle * 5 + ", " + alpha + ")";
     context.clearRect(0, 0, width, height);
     context.beginPath();
-    context.arc(centerX, y, 50, 0, Math.PI * 2, false);
+    context.arc(centerX, y, radius, 0, Math.PI * 2, false);
+    context.stroke();
     context.fill();
 
     angle += speed;
