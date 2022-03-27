8ae1ad8486613d9f2c04978ac9c37a18d1eae9a4
iseneld <84731866+iseneld@users.noreply.github.com>
2022-03-27 11:53:06 +0200

    Arrow rendering.

---
+++ trig05_arctangent.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+  let canvas = document.querySelector("#canvas"),
+    context = canvas.getContext("2d"),
+    width = (canvas.width = window.innerWidth),
+    height = (canvas.height = window.innerHeight),
+    arrowX = width / 2,
+    arrowY = height / 2,
+    dx,
+    dy,
+    angle = 0;
+
+  render();
+
+  function render() {
+    context.clearRect(0, 0, width, height);
+
+    context.save();
+    context.translate(arrowX, arrowY);
+    context.rotate(angle);
+
+    context.beginPath();
+    context.moveTo(20, 0);
+    context.lineTo(-20, 0);
+    context.moveTo(20, 0);
+    context.lineTo(10, -10);
+    context.moveTo(20, 0);
+    context.lineTo(10, 10);
+    context.stroke();
+
+    context.restore();
+    requestAnimationFrame(render);
+  }
+};
